8d0cb017e6b5c309773bf08906a4c1baab09e804
olekslukian <alexlukianoff@gmail.com>
2025-10-02 09:51:20 +0200

    Hotfix: Fix wrong face texture rotation

--- src/KekLib3D.Voxels/Rendering/VoxelMesher.cs
+++ src/KekLib3D.Voxels/Rendering/VoxelMesher.cs
@@ -71,10 +71,10 @@ public static class VoxelMesher
         short baseIndex = 0;
 
         Vector2[] faceUVs = [
+            new Vector2(0, 1),
             new Vector2(0, 0),
             new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1)
+            new Vector2(1, 1)
         ];
 
         foreach (var (pos, voxelId) in map.Voxels)
